9d6577d572bf64c9edb32ae7d905063cc841f0a2
Marcio Matteucci <marcio.matteucci@gmail.com>
2017-12-05 22:00:06 -0300

    express-validator y roles

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,10 +1,52 @@
 const router = require('express').Router();
+const { check, validationResult } = require('express-validator/check');
+const { sanitize } = require('express-validator/filter');
 
 const usersController = require('../controllers/usersController');
 const auth = require('../middlewares/authMiddleware');
 
-router.post('/signUp', [], usersController.signUp);
+function checkErrors(req, res, next) {
+   const errors = validationResult(req);
 
-router.post('/signIn', [], usersController.signIn);
+   if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.mapped() });
+   }
+
+   next();
+}
+
+router.post('/signUp', [
+   sanitize('name').trim(),
+   sanitize('name').escape(),
+   check('name', 'El nombre es requerido').exists(),
+   check('name', 'El nombre debe entre 2 y 30 caracteres').isLength({ min: 2, max: 30 }),
+   sanitize('lastname').trim(),
+   sanitize('lastname').escape(),
+   check('lastname', 'El apellido es requerido').exists(),
+   check('lastname', 'El apellido debe entre 2 y 50 caracteres').isLength({ min: 2, max: 50 }),
+   sanitize('username').trim(),
+   sanitize('username').escape(),
+   check('username', 'El usuario es requerido').exists(),
+   check('username', 'El usuario debe entre 3 y 50 caracteres').isLength({ min: 3, max: 50 }),
+   sanitize('password').trim(),
+   sanitize('password').escape(),
+   check('password', 'La contraseña es requerida').exists(),
+   check('password', 'La contraseña debe entre 6 y 30 caracteres').isLength({ min: 6, max: 30 }),
+   check('password', 'La contraseña no puede tener caracteres especiales (solo letras y números)').isAlphanumeric(),
+   sanitize('email').trim(),
+   sanitize('email').escape(),
+   check('email', 'El email es requerido').exists(),
+   check('email', 'No es un formato de email válido').isEmail(),
+   check('role').optional()
+], checkErrors, usersController.signUp);
+
+router.post('/signIn', [
+   sanitize('username').trim(),
+   sanitize('username').escape(),
+   check('username', 'El usuario es requerido').not().isEmpty(),
+   sanitize('password').trim(),
+   sanitize('password').escape(),
+   check('password', 'La contraseña es requerida').not().isEmpty(),
+], checkErrors, usersController.signIn);
 
 module.exports = router;
--- api/routes/testRoutes.js
+++ api/routes/testRoutes.js
@@ -1,12 +1,27 @@
 const router = require('express').Router();
+const { header, validationResult } = require('express-validator/check');
 
 const auth = require('../middlewares/authMiddleware');
 
-router.get('/user', [], auth.isAuth, (req, res) => {
+function checkErrors(req, res, next) {
+   const errors = validationResult(req);
+
+   if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.mapped() });
+   }
+
+   next();
+}
+
+router.get('/user', [
+   header('Authorization', 'Se debe proveer un Token').not().isEmpty()
+], checkErrors, auth.isAuth, (req, res) => {
    res.status(200).json({ success: true, msg: 'Accediste a una ruta con rol de user' });
 });
 
-router.get('/admin', [], auth.isAdmin, (req, res) => {
+router.get('/admin', [
+   header('Authorization', 'Se debe proveer un Token').not().isEmpty()
+], checkErrors, auth.isAdmin, (req, res) => {
    res.status(200).json({ success: true, msg: 'Accediste a una ruta con rol de admin' });
 });
 
