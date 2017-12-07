0c2f4ad87f29d8e3bb4fa3c4978e0cbc0fb54200
Marcio Matteucci <marcio.matteucci@gmail.com>
2017-12-07 17:29:31 -0300

    Cambio en el error handler

--- api/app.js
+++ api/app.js
@@ -22,7 +22,7 @@ app.use('/api', routes);
 
 // Error handling
 app.use((req, res, next) => {
-   const error = new Error('Not found');
+   const error = new Error('Endpoint inválido');
    error.status = 404;
    next(error);
 });
