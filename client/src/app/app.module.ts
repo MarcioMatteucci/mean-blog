173ba3a8164d20a186e51ce4c00c94cf783b1783
Marcio Matteucci <marcio.matteucci@gmail.com>
2017-12-14 20:10:42 -0300

    Preparando bulma

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NewUserComponent } from './components/new-user/new-user.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
