040dc38deb00fe3514356c8f6c5c910f57a73e8e
Marcio Matteucci <marcio.matteucci@gmail.com>
2017-12-16 19:38:10 -0300

    Confirm password validation

--- client/src/app/components/sign-up/sign-up.component.ts
+++ client/src/app/components/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
 import { User } from './../../models/user.model';
 
@@ -11,7 +11,59 @@ import { User } from './../../models/user.model';
 })
 export class SignUpComponent implements OnInit {
 
+  // Form
   signUpForm: FormGroup;
+
+  // Form Controls
+  name = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(30)
+    ],
+    updateOn: 'change'
+  });
+  lastname = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(50)
+    ],
+    updateOn: 'change'
+  });
+  username = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50)
+    ],
+    updateOn: 'change'
+  });
+  password = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(30),
+      Validators.pattern('^[a-zA-Z0-9]+$')
+    ],
+    updateOn: 'change'
+  });
+  confirmPassword = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(30)
+    ],
+    updateOn: 'change'
+  });
+  email = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.email
+    ],
+    updateOn: 'change'
+  });
+
 
   constructor(
 
@@ -21,54 +73,18 @@ export class SignUpComponent implements OnInit {
 
   createSignUpForm() {
     this.signUpForm = new FormGroup({
-      name: new FormControl('', {
-        validators: [
-          Validators.required,
-          Validators.minLength(2),
-          Validators.maxLength(30)
-        ],
-        updateOn: 'change'
-      }),
-      lastname: new FormControl('', {
-        validators: [
-          Validators.required,
-          Validators.minLength(2),
-          Validators.maxLength(50)
-        ],
-        updateOn: 'change'
-      }),
-      username: new FormControl('', {
-        validators: [
-          Validators.required,
-          Validators.minLength(3),
-          Validators.maxLength(50)
-        ],
-        updateOn: 'change'
-      }),
-      password: new FormControl('', {
-        validators: [
-          Validators.required,
-          Validators.minLength(6),
-          Validators.maxLength(30)
-        ],
-        updateOn: 'change'
-      }),
-      confirmPassword: new FormControl('', {
-        validators: [
-          Validators.required,
-          Validators.minLength(6),
-          Validators.maxLength(30)
-        ],
-        updateOn: 'change'
-      }),
-      email: new FormControl('', {
-        validators: [
-          Validators.required,
-          Validators.email
-        ],
-        updateOn: 'change'
-      })
-    });
+      name: this.name,
+      lastname: this.lastname,
+      username: this.username,
+      password: this.password,
+      confirmPassword: this.confirmPassword,
+      email: this.email
+    }, this.passwordsMatchValidator);
+  }
+
+  passwordsMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value
+      ? null : { 'passwordsDontMatch': true };
   }
 
   ngOnInit() {
