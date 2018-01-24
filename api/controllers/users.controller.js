7fe66832a7e55fad1c3a6a430f6817a6cb0ad74e
Marcio Matteucci <marcio.matteucci@gmail.com>
2018-01-24 20:28:09 -0300

    Refactor usersController (try/catch y async/await)

--- api/controllers/users.controller.js
+++ api/controllers/users.controller.js
@@ -8,34 +8,45 @@ module.exports = {
    =====================*/
    signUp: async (req, res) => {
 
-      // Datos del body
-      const { name, lastname, username, password, role } = await req.body;
-      const email = await req.body.email.toLowerCase();
+      try {
+         // Declaro 2 promesas que van a correr en paralelo
+         const sameEmailUserPromise = User.findOne({ email: req.body.email.toLowerCase() }).exec();
+         const sameUsernameUserPromise = User.findOne({ username: req.body.username }).exec();
 
-      // Validar que el email y username esten disponibles
-      const sameEmailUser = await User.findOne({ email });
-      if (sameEmailUser) {
-         return res.status(403).json({ msg: 'El email está en uso' });
-      }
+         // Corro las 2 promesas en paralelo y espero q terminen ambas
+         const [sameEmailUser, sameUsernameUser] = await Promise.all([sameEmailUserPromise, sameUsernameUserPromise]);
 
-      const sameUsernameUser = await User.findOne({ username });
-      if (sameUsernameUser) {
-         return res.status(403).json({ msg: 'El nombre de usuario está en uso' });
-      }
+         // Valido que el email y el username no esten en uso
+         if (sameEmailUser) {
+            return res.status(422).json({ msg: 'El email está en uso' });
+         }
+         if (sameUsernameUser) {
+            return res.status(422).json({ msg: 'El nombre de usuario está en uso' });
+         }
 
-      // Crea nuevo usuario con los datos del body
-      const newUser = await new User({ name, lastname, username, password, email, role });
-
-      // Persistencia del nuevo usuario
-      await newUser.save()
-         .then(async (user) => {
-            const tokenInfo = await jwtService.signToken(user);
-            return res.status(201).json({ msg: 'Usuario creado', user, tokenInfo });
-         })
-         .catch(async (err) => {
-            console.log(err);
-            return res.status(500).json({ msg: 'Error al guardar el nuevo usuario', error: err });
+         // Espero para q se cree el nuevo usuario
+         const user = await new User({
+            name: req.body.name,
+            lastname: req.body.lastname,
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email.toLowerCase(),
+            role: req.body.role
          });
+
+         // Declaro 2 promesas q pueden correr en paralelo
+         const newUserPromise = user.save();
+         const tokenInfoPromise = jwtService.signToken(user);
+
+         // Corro las 2 promesas en paralelo y espero a q terminen ambas
+         const [newUser, tokenInfo] = await Promise.all([newUserPromise, tokenInfoPromise]);
+
+         res.status(201).json({ msg: 'Usuario creado', user: newUser, tokenInfo });
+
+      } catch (err) {
+         console.error(err);
+         res.status(500).json({ msg: 'Error al crear usuario', error: err });
+      }
 
    },
 
@@ -44,30 +55,33 @@ module.exports = {
    ===============*/
    signIn: async (req, res) => {
 
-      // Datos del body
-      const { username, password } = await req.body;
+      try {
+         // Espero hasta encontrar el usuario
+         const user = await User.findOne({ username: req.body.username })
+            .exec();
 
-      User.findOne({ username }).exec()
-         .then(async (user) => {
-            if (!user) {
-               return res.status(404).json({ msg: 'Nombre de usuario y/o contraseña incorrectos' });
-            }
+         if (!user) {
+            return res.status(401).json({ msg: 'Nombre de usuario y/o contraseña incorrectos' });
+         }
 
-            if (user) {
-               const isMatch = await user.isValidPassword(password);
+         // Espero hasta validar la password
+         const isMatch = await user.isValidPassword(req.body.password);
 
-               if (!isMatch) {
-                  return res.status(404).json({ msg: 'Nombre de usuario y/o contraseña incorrectos' });
-               }
+         if (!isMatch) {
+            return res.status(401).json({ msg: 'Nombre de usuario y/o contraseña incorrectos' });
+         }
 
-               const token = await jwtService.signToken(user);
+         // Espero hasta generar el token
+         const tokenInfo = await jwtService.signToken(user);
 
-               res.status(200).json({ msg: 'Login satisfactorio', token });
-            }
-         })
-         .catch((err) => {
-            return res.status(500).json({ msg: 'Error al obtener el usuario', error: err });
-         });
+         user.set({ password: ':)' });
+
+         res.status(200).json({ msg: 'Login satisfactorio', user, tokenInfo });
+
+      } catch (err) {
+         console.error(err);
+         res.status(500).json({ msg: 'Error al iniciar sesión', error: err });
+      }
 
    },
 
@@ -76,19 +90,21 @@ module.exports = {
    ===================*/
    checkUsername: async (req, res) => {
 
-      const username = await req.query.username;
+      try {
+         // Espero hasta encontrar (o no) al usuario
+         const sameUsernameUser = await User.findOne({ username: req.query.username }).exec()
 
-      User.findOne({ username }).exec()
-         .then(async (user) => {
-            if (user) {
-               return res.status(200).json({ isUsernameAvailable: false, msg: 'El nombre de usuario está en uso' });
-            } else if (!user) {
-               return res.status(200).json({ isUsernameAvailable: true, msg: 'El nombre de usario está disponible' });
-            }
-         })
-         .catch((err) => {
-            return res.status(500).json({ msg: 'Error al obtener el usuario', error: err });
-         });
+         if (sameUsernameUser) {
+            return res.status(200).json({ isUsernameAvailable: false, msg: 'El nombre de usuario está en uso' });
+         } else if (!sameUsernameUser) {
+            return res.status(200).json({ isUsernameAvailable: true, msg: 'El nombre de usario está disponible' });
+         }
+
+      } catch (err) {
+         console.error(err);
+         res.status(500).json({ msg: 'Error al buscar disponibilidad de nombre de usuario', error: err });
+      }
+
    },
 
    /*================
@@ -96,19 +112,20 @@ module.exports = {
    =================*/
    checkEmail: async (req, res) => {
 
-      const email = await req.query.email;
+      try {
+         // Espero hasta encontrar (o no) al usuario
+         const sameEmailUser = await User.findOne({ email: req.query.email }).exec()
 
-      User.findOne({ email }).exec()
-         .then(async (user) => {
-            if (user) {
-               return res.status(200).json({ isEmailAvailable: false, msg: 'El email está en uso' });
-            } else if (!user) {
-               return res.status(200).json({ isEmailAvailable: true, msg: 'El email está disponible' });
-            }
-         })
-         .catch((err) => {
-            return res.status(500).json({ msg: 'Error al obtener el usuario', error: err });
-         });
+         if (sameEmailUser) {
+            return res.status(200).json({ isEmailAvailable: false, msg: 'El email está en uso' });
+         } else if (!sameEmailUser) {
+            return res.status(200).json({ isEmailAvailable: true, msg: 'El email está disponible' });
+         }
+
+      } catch (err) {
+         console.error(err);
+         res.status(500).json({ msg: 'Error al buscar disponibilidad de nombre de usuario', error: err });
+      }
 
    }
 
--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { check, validationResult, query } = require('express-validator/check');
+const { check, body, validationResult, query } = require('express-validator/check');
 const { sanitize } = require('express-validator/filter');
 
 const usersController = require('../controllers/users.controller');
@@ -9,44 +9,38 @@ function checkErrors(req, res, next) {
    const errors = validationResult(req);
 
    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
+      return res.status(422).json({ errors: errors.mapped() });
    }
 
    next();
 }
 
 router.post('/signUp', [
-   sanitize('name').trim(),
-   sanitize('name').escape(),
-   check('name', 'El nombre es requerido').exists(),
-   check('name', 'El nombre debe entre 2 y 30 caracteres').isLength({ min: 2, max: 30 }),
-   sanitize('lastname').trim(),
-   sanitize('lastname').escape(),
-   check('lastname', 'El apellido es requerido').exists(),
-   check('lastname', 'El apellido debe entre 2 y 50 caracteres').isLength({ min: 2, max: 50 }),
-   sanitize('username').trim(),
-   sanitize('username').escape(),
-   check('username', 'El usuario es requerido').exists(),
-   check('username', 'El usuario debe entre 3 y 50 caracteres').isLength({ min: 3, max: 50 }),
-   sanitize('password').trim(),
-   sanitize('password').escape(),
-   check('password', 'La contraseña es requerida').exists(),
-   check('password', 'La contraseña debe entre 6 y 30 caracteres').isLength({ min: 6, max: 30 }),
-   check('password', 'La contraseña no puede tener caracteres especiales (solo letras y números)').isAlphanumeric(),
-   sanitize('email').trim(),
-   sanitize('email').escape(),
-   check('email', 'El email es requerido').exists(),
-   check('email', 'No es un formato de email válido').isEmail(),
-   check('role').optional()
+   sanitize('name').trim().escape(),
+   body('name', 'El nombre es requerido').exists(),
+   body('name', 'El nombre debe entre 2 y 30 caracteres').isLength({ min: 2, max: 30 }),
+   sanitize('lastname').trim().escape(),
+   body('lastname', 'El apellido es requerido').exists(),
+   body('lastname', 'El apellido debe entre 2 y 50 caracteres').isLength({ min: 2, max: 50 }),
+   sanitize('username').trim().escape(),
+   body('username', 'El usuario es requerido').exists(),
+   body('username', 'El usuario debe entre 3 y 50 caracteres').isLength({ min: 3, max: 50 }),
+   sanitize('password').trim().escape(),
+   body('password', 'La contraseña es requerida').exists(),
+   body('password', 'La contraseña debe entre 6 y 30 caracteres').isLength({ min: 6, max: 30 }),
+   body('password', 'La contraseña no puede tener caracteres especiales (solo letras y números)').isAlphanumeric(),
+   sanitize('email').trim().escape(),
+   body('email', 'El email es requerido').exists(),
+   body('email', 'No es un formato de email válido').isEmail(),
+   sanitize('role').trim().escape(),
+   body('role', 'No es un rol válido').optional().isIn(['admin', 'user'])
 ], checkErrors, usersController.signUp);
 
 router.post('/signIn', [
-   sanitize('username').trim(),
-   sanitize('username').escape(),
-   check('username', 'El usuario es requerido').exists(),
-   sanitize('password').trim(),
-   sanitize('password').escape(),
-   check('password', 'La contraseña es requerida').exists(),
+   sanitize('username').trim().escape(),
+   body('username', 'El usuario es requerido').exists(),
+   sanitize('password').trim().escape(),
+   body('password', 'La contraseña es requerida').exists(),
 ], checkErrors, usersController.signIn);
 
 router.get('/checkUsername', [
@@ -58,6 +52,5 @@ router.get('/checkEmail', [
    query('email', 'El email es requerido').exists(),
    query('email', 'No es un formato de email válido').isEmail(),
 ], checkErrors, usersController.checkEmail);
-
 
 module.exports = router;
